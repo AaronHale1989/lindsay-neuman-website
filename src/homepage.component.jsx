53569ed4986cf429bb27a88a2ad6903cbd288228
AaronHale1989 <halea02130@gmail.com>
2019-10-22 14:39:06 -0400

    initial setup

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='main-container'>
+            <div className='photo-container'>
+                <div className='photo'>
+                    <h1>PHOTO</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
